eb4ab853e985719148e4ea959a4c750761e54045
ZYL <2069809799@qq.com>
2020-05-05 12:52:18 +0800

    reducing redundancy

--- 1.py
+++ 1.py
@@ -1,7 +1,7 @@
 from mirai import Mirai, Plain, MessageChain, Group, Member, At, Source, Image
-from mirai.face import QQFaces
-from mirai import Face
-import asyncio
+# from mirai.face import QQFaces
+# from mirai import Face
+# import asyncio
 import requests
 from ostest import picbase, randompic
 
@@ -12,24 +12,24 @@ url2 = "https://chp.shadiao.app/api.php?from=sunbelife"
 urlp = "https://ss2.bdstatic.com/70cFvnSh_Q1YnxGkpoWK1HF6hhy/it/u=3252521864,872614242&fm=26&gp=0.jpg"
 qq = 3207033875  # 字段 qq 的值
 authKey = '1234567890'  # 字段 authKey 的值
-mirai_api_http_locate = 'localhost:8080/'  # httpapi所在主机的地址端口,如果 setting.yml 文件里字段 "enableWebsocket" 的值为 "true" 则需要将 "/" 换成 "/ws", 否则将接收不到消息.
+mirai_api_http_locate = 'localhost:8080/'
+# httpapi所在主机的地址端口,如果 setting.yml 文件里字段 "enableWebsocket" 的值为 "true" 则需要将 "/" 换成 "/ws", 否则将接收不到消息.
 
 app = Mirai(f"mirai://{mirai_api_http_locate}?authKey={authKey}&qq={qq}")
 
 
 @app.receiver("GroupMessage")
-async def quote(app: Mirai, group: Group, message: MessageChain, member: Member, source: Source):
+async def quote(app: Mirai, group: Group, message: MessageChain,
+                member: Member, source: Source):
     if message.toString().find("骂我") != -1:
         txt1 = requests.get(url1)
-        await app.sendGroupMessage(group, [At(member.id),
-                                           Plain(text=txt1.text)],
-                                   quoteSource=source)
+        await app.sendGroupMessage(
+            group, [At(member.id), Plain(text=txt1.text)], quoteSource=source)
         return True
     if message.toString().find("夸我") != -1:
         txt2 = requests.get(url2)
-        await app.sendGroupMessage(group, [At(member.id),
-                                           Plain(text=txt2.text)],
-                                   quoteSource=source)
+        await app.sendGroupMessage(
+            group, [At(member.id), Plain(text=txt2.text)], quoteSource=source)
         return True
     if message.toString().find("tu") != -1:
         pic = picbase()
--- ostest.py
+++ ostest.py
@@ -1,10 +1,24 @@
+#随机选择
+
 import base64
 from tupian import randompic
+from os import listdir
+import random
 
 # import os,sys
 # project_path = os.path.dirname(os.path.abspath(__file__)) # 获取当前文件路径的上一级目录
-num = randompic()
-dir = './PixivImage/' + num
+
+
+def listd():
+    dataset_list = listdir("PixivImage")
+    return dataset_list
+
+
+def randompic():
+    dataset_list = listd()
+    length = len(dataset_list)
+    output = dataset_list[random.randint(0, length)]
+    return output
 
 
 def picbase():
@@ -12,3 +26,7 @@ def picbase():
         base64_data = base64.b64encode(f.read())
         s = base64_data.decode()
     return s
+
+
+num = randompic()
+dir = './PixivImage/' + num
